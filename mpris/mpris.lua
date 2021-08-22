733beae28759ba4005cd8d5a172d8dec65c62ac8
drakkar1969 <nicolabay@outlook.com>
2021-08-22 17:05:52 +0200

    Use table for text settings in MPRIS conkys

--- mpris/mpris.lua
+++ mpris/mpris.lua
@@ -2,23 +2,45 @@
 -- Variables
 ---------------------------------------
 player_name="Lollypop"
+
 image_size=80
 frame_padding=1
 frame_color=0x383c4a
 frame_alpha=0.7
+
 text_font="Ubuntu"
-text_size_title=24
-text_size_artist=18
-text_size_pos=13
-text_color_title=0x383c4a
-text_color_artist=0x21232b
-text_color_pos=0x4d5366
-x_title=image_size+2*frame_padding+20
-y_title=23
-x_artist=x_title
-y_artist=y_title+28
-x_pos=x_title
-y_pos=y_title+53
+text_x=image_size+2*frame_padding+20
+text_y=23
+
+---------------------------------------
+-- Text table
+---------------------------------------
+text_table = {
+	title = {
+		text="title",
+		font=text_font,
+		font_size=24,
+		color=0x383c4a,
+		alpha=1,
+		x=text_x, y=text_y
+	},
+	artist = {
+		text="artist",
+		font=text_font,
+		font_size=18,
+		color=0x21232b,
+		alpha=1,
+		x=text_x, y=text_y+28
+	},
+	pos = {
+		text="status",
+		font=text_font,
+		font_size=13,
+		color=0x4d5366,
+		alpha=1,
+		x=text_x, y=text_y+53
+	}
+}
 
 ---------------------------------------
 -- LUA FUNCTIONS
@@ -68,10 +90,15 @@ end
 ---------------------------------------
 -- Function draw_text
 ---------------------------------------
-function draw_text(cr, x, y, text, f_size, color)
-	cairo_select_font_face (cr, text_font, CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL)
-	cairo_set_font_size (cr, f_size)
-	cairo_set_source_rgba (cr, rgb_to_r_g_b(color, 1))
+function draw_text(cr,pt)
+	text=pt['text']
+	font,fs=pt['font'],pt['font_size']
+	color,alpha=pt['color'],pt['alpha']
+	x,y=pt['x'],pt['y']
+
+	cairo_select_font_face (cr, font, CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL)
+	cairo_set_font_size (cr, fs)
+	cairo_set_source_rgba (cr, rgb_to_r_g_b(color, alpha))
 	cairo_move_to (cr, x, y)
 	cairo_show_text (cr, text)
 	cairo_stroke (cr)
@@ -83,13 +110,14 @@ end
 function conky_albumart()
 	if conky_window==nil then return end
 
+	-- Get metadata
 	local metadata = conky_parse(string.format("${exec 'playerctl metadata --player=%s 2>/dev/null'}", player_name))
 
 	if metadata==nil then return end
 
-	local meta_title = conky_parse(string.format("${exec 'playerctl metadata --player=%s --format \"{{ uc(title) }}\"'}", player_name))
-	local meta_artist = conky_parse(string.format("${exec 'playerctl metadata --player=%s --format \"{{ uc(artist) }}\"'}", player_name))
-	local meta_pos = conky_parse(string.format("${exec 'playerctl metadata --player=%s --format \"{{ uc(status) }}: {{ duration(position) }} | {{ duration(mpris:length) }}\"'}", player_name))
+	text_table['title']['text'] = conky_parse(string.format("${exec 'playerctl metadata --player=%s --format \"{{ uc(title) }}\"'}", player_name))
+	text_table['artist']['text'] = conky_parse(string.format("${exec 'playerctl metadata --player=%s --format \"{{ uc(artist) }}\"'}", player_name))
+	text_table['pos']['text'] = conky_parse(string.format("${exec 'playerctl metadata --player=%s --format \"{{ uc(status) }}: {{ duration(position) }} | {{ duration(mpris:length) }}\"'}", player_name))
 
 	local meta_art = conky_parse(string.format("${exec 'playerctl metadata --player=Lollypop --format \"{{ mpris:artUrl }}\"'}", player_name))
 	meta_art = meta_art:gsub("file:%/%/","")
@@ -98,11 +126,13 @@ function conky_albumart()
 
 	local cr=cairo_create(cs)
 
+	-- Draw cover with frame
 	draw_imlib2_image(cr, meta_art)
 
-	draw_text(cr, x_title, y_title, meta_title, text_size_title, text_color_title)
-	draw_text(cr, x_artist, y_artist, meta_artist, text_size_artist, text_color_artist)
-	draw_text(cr, x_pos, y_pos, meta_pos, text_size_pos, text_color_pos)
+	-- Draw text
+	draw_text(cr, text_table['title'])
+	draw_text(cr, text_table['artist'])
+	draw_text(cr, text_table['pos'])
 
 	cairo_destroy(cr)
 	cairo_surface_destroy(cs)
