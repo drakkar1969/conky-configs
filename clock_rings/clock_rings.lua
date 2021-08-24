cbb0e001e5e4307b956526ff374236b351f750ab
drakkar1969 <nicolabay@outlook.com>
2021-08-24 22:10:06 +0200

    Lua code cleanup: clock_rings conky

--- clock_rings/clock_rings.lua
+++ clock_rings/clock_rings.lua
@@ -15,18 +15,18 @@ ring_alpha_fg_dummy=0
 
 cpu_start_angle=91
 cpu_end_angle=209
-cpu_thickness=4
+cpu_width=4
 cpu_radius=ring_radius+9
 cpu_spacing=6
 
 memtemp_radius=ring_radius+18
-memtemp_thickness=22
+memtemp_width=22
 
 fs_radius=ring_radius+37
-fs_thickness=6
+fs_width=6
 
 dummy_radius=ring_radius+60
-dummy_thickness=2
+dummy_width=2
 
 ---------------------------------------
 -- Clock variables
@@ -53,8 +53,8 @@ line_dotted_alpha=0.70
 ---------------------------------------
 graph_color=0x383c4a
 graph_alpha=0.7
-graph_thickness=1
-graph_spacing=0
+graph_bar_width=1
+graph_bar_gap=0
 
 ---------------------------------------
 -- NET variables
@@ -72,13 +72,13 @@ rings_table = {
 		name='time',
 		arg='%S',
 		max=60,
-		bg_colour=ring_color_bg,
+		bg_color=ring_color_bg,
 		bg_alpha=ring_alpha_bg,
-		fg_colour=ring_color_fg,
+		fg_color=ring_color_fg,
 		fg_alpha=ring_alpha_fg,
 		x=ring_x, y=ring_y,
 		radius=ring_radius,
-		thickness=4,
+		width=4,
 		start_angle=0,
 		end_angle=360
 	},
@@ -87,13 +87,13 @@ rings_table = {
 		name='cpu',
 		arg='cpu1',
 		max=100,
-		bg_colour=ring_color_bg,
+		bg_color=ring_color_bg,
 		bg_alpha=ring_alpha_bg,
-		fg_colour=ring_color_fg,
+		fg_color=ring_color_fg,
 		fg_alpha=ring_alpha_fg,
 		x=ring_x, y=ring_y,
 		radius=cpu_radius,
-		thickness=cpu_thickness,
+		width=cpu_width,
 		start_angle=cpu_start_angle,
 		end_angle=cpu_end_angle
 	},
@@ -101,13 +101,13 @@ rings_table = {
 		name='cpu',
 		arg='cpu2',
 		max=100,
-		bg_colour=ring_color_bg,
+		bg_color=ring_color_bg,
 		bg_alpha=ring_alpha_bg,
-		fg_colour=ring_color_fg,
+		fg_color=ring_color_fg,
 		fg_alpha=ring_alpha_fg,
 		x=ring_x, y=ring_y,
 		radius=cpu_radius+cpu_spacing,
-		thickness=cpu_thickness,
+		width=cpu_width,
 		start_angle=cpu_start_angle,
 		end_angle=cpu_end_angle
 	},
@@ -115,13 +115,13 @@ rings_table = {
 		name='cpu',
 		arg='cpu3',
 		max=100,
-		bg_colour=ring_color_bg,
+		bg_color=ring_color_bg,
 		bg_alpha=ring_alpha_bg,
-		fg_colour=ring_color_fg,
+		fg_color=ring_color_fg,
 		fg_alpha=ring_alpha_fg,
 		x=ring_x, y=ring_y,
 		radius=cpu_radius+2*cpu_spacing,
-		thickness=cpu_thickness,
+		width=cpu_width,
 		start_angle=cpu_start_angle,
 		end_angle=cpu_end_angle
 	},
@@ -129,13 +129,13 @@ rings_table = {
 		name='cpu',
 		arg='cpu4',
 		max=100,
-		bg_colour=ring_color_bg,
+		bg_color=ring_color_bg,
 		bg_alpha=ring_alpha_bg,
-		fg_colour=ring_color_fg,
+		fg_color=ring_color_fg,
 		fg_alpha=ring_alpha_fg,
 		x=ring_x, y=ring_y,
 		radius=cpu_radius+3*cpu_spacing,
-		thickness=cpu_thickness,
+		width=cpu_width,
 		start_angle=cpu_start_angle,
 		end_angle=cpu_end_angle
 	},
@@ -144,13 +144,13 @@ rings_table = {
 		name='memperc',
 		arg='',
 		max=100,
-		bg_colour=ring_color_bg,
+		bg_color=ring_color_bg,
 		bg_alpha=ring_alpha_bg,
-		fg_colour=ring_color_fg,
+		fg_color=ring_color_fg,
 		fg_alpha=ring_alpha_fg,
 		x=ring_x, y=ring_y,
 		radius=memtemp_radius,
-		thickness=memtemp_thickness,
+		width=memtemp_width,
 		start_angle=212,
 		end_angle=329
 	},
@@ -159,13 +159,13 @@ rings_table = {
 		name='acpitemp',
 		arg='',
 		max=100,
-		bg_colour=ring_color_bg,
+		bg_color=ring_color_bg,
 		bg_alpha=ring_alpha_bg,
-		fg_colour=ring_color_fg,
+		fg_color=ring_color_fg,
 		fg_alpha=ring_alpha_fg,
 		x=ring_x, y=ring_y,
 		radius=memtemp_radius,
-		thickness=memtemp_thickness,
+		width=memtemp_width,
 		start_angle=-28,
 		end_angle=88
 	},
@@ -174,13 +174,13 @@ rings_table = {
 		name='fs_used_perc',
 		arg='/',
 		max=100,
-		bg_colour=ring_color_bg,
+		bg_color=ring_color_bg,
 		bg_alpha=ring_alpha_bg,
-		fg_colour=ring_color_fg,
+		fg_color=ring_color_fg,
 		fg_alpha=ring_alpha_fg,
 		x=ring_x, y=ring_y,
 		radius=fs_radius,
-		thickness=fs_thickness,
+		width=fs_width,
 		start_angle=-120,
 		end_angle=-2
 	},
@@ -188,13 +188,13 @@ rings_table = {
 		name='fs_used_perc',
 		arg='/home/data',
 		max=100,
-		bg_colour=ring_color_bg,
+		bg_color=ring_color_bg,
 		bg_alpha=ring_alpha_bg,
-		fg_colour=ring_color_fg,
+		fg_color=ring_color_fg,
 		fg_alpha=ring_alpha_fg,
 		x=ring_x, y=ring_y,
 		radius=fs_radius,
-		thickness=fs_thickness,
+		width=fs_width,
 		start_angle=2,
 		end_angle=120
 	},
@@ -203,13 +203,13 @@ rings_table = {
 		name='cpu', -- dummy (used for arc)
 		arg='',
 		max=1,
-		bg_colour=ring_color_bg,
+		bg_color=ring_color_bg,
 		bg_alpha=ring_alpha_bg_dummy,
-		fg_colour=ring_color_fg,
+		fg_color=ring_color_fg,
 		fg_alpha=ring_alpha_fg_dummy,
 		x=ring_x, y=ring_y,
 		radius=dummy_radius,
-		thickness=dummy_thickness,
+		width=dummy_width,
 		start_angle=73,
 		end_angle=107
 	},
@@ -217,13 +217,13 @@ rings_table = {
 		name='cpu', -- dummy (used for arc)
 		arg='',
 		max=1,
-		bg_colour=ring_color_bg,
+		bg_color=ring_color_bg,
 		bg_alpha=ring_alpha_bg_dummy,
-		fg_colour=ring_color_fg,
+		fg_color=ring_color_fg,
 		fg_alpha=ring_alpha_fg_dummy,
 		x=ring_x+470, y=ring_y,
 		radius=dummy_radius,
-		thickness=dummy_thickness,
+		width=dummy_width,
 		start_angle=73,
 		end_angle=107
 	},
@@ -256,8 +256,8 @@ graphs_table = {
 		alpha=graph_alpha,
 		x=ring_x+ring_radius+78, y=ring_y-1,
 		w=432, h=26,
-		thickness=graph_thickness,
-		spacing=graph_spacing,
+		bar_w=graph_bar_width,
+		bar_gap=graph_bar_gap,
 		data={}
 	}
 }
@@ -270,22 +270,20 @@ require 'cairo'
 ---------------------------------------
 -- Function rgb_to_r_g_b
 ---------------------------------------
-function rgb_to_r_g_b(colour,alpha)
-	return ((colour/0x10000)%0x100)/255.,((colour/0x100)%0x100)/255.,(colour%0x100)/255.,alpha
+function rgb_to_r_g_b(color,alpha)
+	return(((color/0x10000)%0x100)/255.,((color/0x100)%0x100)/255.,(color%0x100)/255.,alpha)
 end
 
 ---------------------------------------
 -- Function get_conky_string
 ---------------------------------------
 function get_conky_string(name,arg)
-	local str=''
-	local value=0
 
-	str=string.format('${%s %s}',name,arg)
+	local str=string.format('${%s %s}',name,arg)
 	str=conky_parse(str)
 
-	value=tonumber(str)
-	if value == nil then value = 0 end
+	local value=tonumber(str)
+	if value == nil then value=0 end
 
 	return(value)
 end
@@ -294,27 +292,24 @@ end
 -- Function draw_ring
 ---------------------------------------
 function draw_ring(cr,pt)
-	local xc,yc,ring_r,ring_w,sa,ea=pt['x'],pt['y'],pt['radius'],pt['thickness'],pt['start_angle'],pt['end_angle']
-	local bgc, bga, fgc, fga=pt['bg_colour'], pt['bg_alpha'], pt['fg_colour'], pt['fg_alpha']
+	local value=get_conky_string(pt.name,pt.arg)
 
-	local value=get_conky_string(pt['name'],pt['arg'])
-
-	local pct=value/pt['max']
+	local pct=value/pt.max
 	pct=(pct > 1 and 1 or pct)
 
-	local angle_0=sa*(2*math.pi/360)-math.pi/2
-	local angle_f=ea*(2*math.pi/360)-math.pi/2
+	local angle_0=pt.start_angle*(2*math.pi/360)-math.pi/2
+	local angle_f=pt.end_angle*(2*math.pi/360)-math.pi/2
 	local t_arc=pct*(angle_f-angle_0)
 
 	-- Draw background ring
-	cairo_arc(cr,xc,yc,ring_r,angle_0,angle_f)
-	cairo_set_source_rgba(cr,rgb_to_r_g_b(bgc,bga))
-	cairo_set_line_width(cr,ring_w)
+	cairo_arc(cr,pt.x,pt.y,pt.radius,angle_0,angle_f)
+	cairo_set_source_rgba(cr,rgb_to_r_g_b(pt.bg_color,pt.bg_alpha))
+	cairo_set_line_width(cr,pt.width)
 	cairo_stroke(cr)
 
 	-- Draw indicator ring
-	cairo_arc(cr,xc,yc,ring_r,angle_0,angle_0+t_arc)
-	cairo_set_source_rgba(cr,rgb_to_r_g_b(fgc,fga))
+	cairo_arc(cr,pt.x,pt.y,pt.radius,angle_0,angle_0+t_arc)
+	cairo_set_source_rgba(cr,rgb_to_r_g_b(pt.fg_color,pt.fg_alpha))
 	cairo_stroke(cr)
 end
 
@@ -322,20 +317,17 @@ end
 -- Function draw_clock_hands
 ---------------------------------------
 function draw_clock_hands(cr,xc,yc)
-	local secs,mins,hours,secs_arc,mins_arc,hours_arc
-	local xh,yh,xm,ym,xs,ys
+	local secs=os.date("%S")
+	local mins=os.date("%M")
+	local hours=os.date("%I")
 
-	secs=os.date("%S")
-	mins=os.date("%M")
-	hours=os.date("%I")
-
-	secs_arc=(2*math.pi/60)*secs
-	mins_arc=(2*math.pi/60)*mins+secs_arc/60
-	hours_arc=(2*math.pi/12)*hours+mins_arc/12
+	local secs_arc=(2*math.pi/60)*secs
+	local mins_arc=(2*math.pi/60)*mins+secs_arc/60
+	local hours_arc=(2*math.pi/12)*hours+mins_arc/12
 
 	-- Draw hour hand
-	xh=xc+0.7*clock_r*math.sin(hours_arc)
-	yh=yc-0.7*clock_r*math.cos(hours_arc)
+	local xh=xc+0.7*clock_r*math.sin(hours_arc)
+	local yh=yc-0.7*clock_r*math.cos(hours_arc)
 	cairo_move_to(cr,xc,yc)
 	cairo_line_to(cr,xh,yh)
 
@@ -345,8 +337,8 @@ function draw_clock_hands(cr,xc,yc)
 	cairo_stroke(cr)
 
 	-- Draw minute hand
-	xm=xc+clock_r*math.sin(mins_arc)
-	ym=yc-clock_r*math.cos(mins_arc)
+	local xm=xc+clock_r*math.sin(mins_arc)
+	local ym=yc-clock_r*math.cos(mins_arc)
 	cairo_move_to(cr,xc,yc)
 	cairo_line_to(cr,xm,ym)
 
@@ -355,8 +347,8 @@ function draw_clock_hands(cr,xc,yc)
 
 	-- Draw seconds hand
 	if clock_show_seconds then
-		xs=xc+clock_r*math.sin(secs_arc)
-		ys=yc-clock_r*math.cos(secs_arc)
+		local xs=xc+clock_r*math.sin(secs_arc)
+		local ys=yc-clock_r*math.cos(secs_arc)
 		cairo_move_to(cr,xc,yc)
 		cairo_line_to(cr,xs,ys)
 
@@ -369,10 +361,8 @@ end
 -- Function draw_solid_line
 ---------------------------------------
 function draw_solid_line(cr,pt)
-	local xs,ys,xe,ye=pt['xs'],pt['ys'],pt['xe'],pt['ye']
-
-	cairo_move_to(cr,xs,ys)
-	cairo_line_to(cr,xe,ye)
+	cairo_move_to(cr,pt.xs,pt.ys)
+	cairo_line_to(cr,pt.xe,pt.ye)
 
 	cairo_set_line_cap(cr,CAIRO_LINE_CAP_SQUARE)
 	cairo_set_line_width(cr,line_solid_width)
@@ -384,10 +374,8 @@ end
 -- Function draw_dotted_line
 ---------------------------------------
 function draw_dotted_line(cr,pt)
-	local xs,ys,xe,ye=pt['xs'],pt['ys'],pt['xe'],pt['ye']
-
-	cairo_move_to(cr,xs,ys)
-	cairo_line_to(cr,xe,ye)
+	cairo_move_to(cr,pt.xs,pt.ys)
+	cairo_line_to(cr,pt.xe,pt.ye)
 
 	cairo_set_line_cap(cr,CAIRO_LINE_CAP_ROUND)
 	cairo_set_dash(cr,{1,3},2,0)
@@ -400,44 +388,42 @@ end
 -- Function draw_graph
 ---------------------------------------
 function draw_graph(cr,pt)
-	local data=pt['data']
-	local x,y,w,h=pt['x'],pt['y'],pt['w'],pt['h']
-	local thickness,spacing=pt['thickness'],pt['spacing']
-	local color,alpha=pt['color'],pt['alpha']
-	local max=pt['max']
+	local data=pt.data
 
-	local num_bars=w/(thickness+spacing)
+	local n_bars=pt.w/(pt.bar_w+pt.bar_gap)
 
-	local value=get_conky_string(pt['name'],pt['arg'])
+	local value=get_conky_string(pt.name,pt.arg)
 
-	for i = 1, num_bars do
+	for i=1, n_bars do
 		if data[i+1] == nil then data[i+1]=0 end
 
 		data[i]=data[i+1]
 
-		if i == num_bars then
-			data[num_bars]=value
+		if i == n_bars then
+			data[n_bars]=value
 		end
 	end
 
-	for i = 1, num_bars do
+	local bar_h
+
+	for i=1, n_bars do
 		-- Check that log of data[i] will not be negative
 		if data[i] < 1 then data[i]=1 end
 
 		-- Transform to log scale
-		bar_h=(math.log10(data[i])/math.log10(max))*h
+		bar_h=(math.log10(data[i])/math.log10(pt.max))*pt.h
 
 		-- Check limits of bar height
 		if bar_h < 0 then bar_h=0 end
-		if bar_h > h then bar_h=h end
+		if bar_h > pt.h then bar_h=pt.h end
 
 		-- Draw bar only if at least 1 full pixel height
 		if bar_h >= 1 then
-			cairo_move_to(cr,x+(thickness/2)+((i-1)*(thickness+spacing)),y)
-			cairo_line_to(cr,x+(thickness/2)+((i-1)*(thickness+spacing)),y-bar_h)
+			cairo_move_to(cr,pt.x+(pt.bar_w/2)+((i-1)*(pt.bar_w+pt.bar_gap)),pt.y)
+			cairo_line_to(cr,pt.x+(pt.bar_w/2)+((i-1)*(pt.bar_w+pt.bar_gap)),pt.y-bar_h)
 
-			cairo_set_line_width(cr,thickness)
-			cairo_set_source_rgba(cr,rgb_to_r_g_b(color,alpha))
+			cairo_set_line_width(cr,pt.bar_w)
+			cairo_set_source_rgba(cr,rgb_to_r_g_b(pt.color,pt.alpha))
 			cairo_stroke(cr)
 		end
 	end
@@ -449,9 +435,9 @@ end
 function conky_clock_rings()
 	if conky_window==nil then return end
 
-	net_interface = conky_parse("${if_up ${template1}}${template1}${else}${if_up ${template0}}${template0}${else}none${endif}${endif}")
+	net_interface=conky_parse("${if_up ${template1}}${template1}${else}${if_up ${template0}}${template0}${else}none${endif}${endif}")
 
-	graphs_table['downspeed']['arg'] = net_interface
+	graphs_table.downspeed.arg=net_interface
 
 	local cs=cairo_xlib_surface_create(conky_window.display,conky_window.drawable,conky_window.visual, conky_window.width,conky_window.height)
 
