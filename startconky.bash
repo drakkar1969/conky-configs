a55a5773875a9301469c62c91b6bfb92fcddee4e
drakkar1969 <nicolabay@outlook.com>
2021-08-25 22:11:40 +0200

    Delete sands of time conky

--- startconky.bash
+++ startconky.bash
@@ -7,9 +7,6 @@ sleep 7        # time (in s) for the DE to start; use ~20 for Gnome or KDE, less
 # # Clock Rings
 # conky -d -c $CONKYFOLDER/clock_rings/clock_rings.conf
 
-# # Sands of Time
-# conky -d -c $CONKYFOLDER/sands_time/clock.conf
-
 # # QlockTwo
 # conky -d -c $CONKYFOLDER/qlock_two/qlocktwo.conf
 
