ec052cc4c3bea7254c8d98f5adbb630bcdfc3253
drakkar1969 <nicolabay@outlook.com>
2022-08-02 18:28:18 +0200

    Tweak conky startup delay

--- startconky.bash
+++ startconky.bash
@@ -2,7 +2,7 @@
 
 CONKYFOLDER=$(dirname $(realpath $0))
 
-sleep 7        # time (in s) for the DE to start; use ~20 for Gnome or KDE, less for Xfce/LXDE etc
+sleep 3        # time (in s) for the DE to start; use ~20 for Gnome or KDE, less for Xfce/LXDE etc
 
 # # Clock Rings
 # conky -d -c $CONKYFOLDER/clock_rings/clock_rings.conf
