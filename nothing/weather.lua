d85b428e062cb06b3fb1c6310f2d9b16e8fc375f
drakkar1969 <nicolabay@outlook.com>
2026-02-21 14:22:38 +0100

    Improve init widget function in nothing weather conky

--- nothing/weather.lua
+++ nothing/weather.lua
@@ -98,6 +98,33 @@ end
 ---------------------------------------
 function init_widget()
 	widget.height = line_spacing * 4 + fonts.text.height * 2 + fonts.caption.height + fonts.time.height + background.padding_y * 2
+
+	widget.time.date_x = widget.x + widget.width - background.padding_x
+	widget.time.date_y = widget.y + background.padding_y
+
+	widget.time.time_x = widget.time.date_x
+	widget.time.time_y = widget.time.date_y + fonts.text.height + line_spacing
+
+	widget.battery.charge_x = widget.time.time_x
+	widget.battery.charge_y = widget.time.time_y + fonts.time.height + line_spacing * 2
+
+	widget.battery.status_x = widget.battery.charge_x
+	widget.battery.status_y = widget.battery.charge_y + fonts.text.height + line_spacing
+
+	widget.weather.temperature_x = widget.x + background.padding_x + widget.weather.icon_size + widget.spacing_x
+	widget.weather.temperature_y = widget.y + background.padding_y + line_spacing * 0.5
+
+	widget.weather.icon_x = widget.x + background.padding_x
+	widget.weather.icon_y = widget.weather.temperature_y + (fonts.weather.height - widget.weather.icon_size)/2
+
+	widget.weather.feels_like_x = widget.weather.icon_x
+	widget.weather.feels_like_y = widget.weather.temperature_y + fonts.weather.height + line_spacing * 1.25
+
+	widget.weather.description_x = widget.weather.feels_like_x
+	widget.weather.description_y = widget.battery.charge_y
+
+	widget.weather.location_x = widget.weather.description_x
+	widget.weather.location_y = widget.battery.status_y
 end
 
 ---------------------------------------
@@ -157,47 +184,25 @@ function conky_main()
 	lib.draw_background(cr, widget, background)
 
 	-- Draw date/time
-	local xt = widget.x + widget.width - background.padding_x
-	local yt = widget.y + background.padding_y
-	local dy
-
-	_, dy = lib.draw_text(cr, fonts.text, ALIGNR, xt, yt, widget.time.date)
-
-	yt = yt + dy + line_spacing
-
-	_, dy = lib.draw_text(cr, fonts.time, ALIGNR, xt, yt, widget.time.time)
+	lib.draw_text(cr, fonts.text, ALIGNR, widget.time.date_x, widget.time.date_y, widget.time.date)
+	lib.draw_text(cr, fonts.time, ALIGNR, widget.time.time_x, widget.time.time_y, widget.time.time)
 
 	-- Draw battery text
-	xb = xt
-	yb = yt + dy + line_spacing * 2
-
-	_, dy = lib.draw_text(cr, fonts.text, ALIGNR, xb, yb, widget.battery.charge)
-
-	lib.draw_text(cr, fonts.caption, ALIGNR, xb, yb + dy + line_spacing, widget.battery.status)
+	lib.draw_text(cr, fonts.text, ALIGNR, widget.battery.charge_x, widget.battery.charge_y, widget.battery.charge)
+	lib.draw_text(cr, fonts.caption, ALIGNR, widget.battery.status_x, widget.battery.status_y, widget.battery.status)
 
 	-- Draw weather icon
-	local xw = widget.x + background.padding_x
-	local yw = widget.y + background.padding_y + line_spacing * 0.5
-
 	if widget.weather.icon ~= '' then
-		cairo_place_image(widget.weather.icon, cr, xw, yw + (fonts.weather.height - widget.weather.icon_size)/2, widget.weather.icon_size, widget.weather.icon_size, 1)
+		cairo_place_image(widget.weather.icon, cr, widget.weather.icon_x, widget.weather.icon_y, widget.weather.icon_size, widget.weather.icon_size, 1)
 	end
 
 	-- Draw weather temperature text
-	_, dy = lib.draw_text(cr, fonts.weather, ALIGNL, xw + widget.weather.icon_size + widget.spacing_x, yw, widget.weather.temperature..'°C')
-
-	yw = yw + dy + line_spacing * 1.25
-
-	_, dy = lib.draw_text(cr, fonts.caption, ALIGNL, xw, yw, 'Feels like '..widget.weather.feels_like..'°C')
+	lib.draw_text(cr, fonts.weather, ALIGNL, widget.weather.temperature_x, widget.weather.temperature_y, widget.weather.temperature..'°C')
+	lib.draw_text(cr, fonts.caption, ALIGNL, widget.weather.feels_like_x, widget.weather.feels_like_y, 'Feels like '..widget.weather.feels_like..'°C')
 
 	-- Draw weather status text
-	yw = yb
-
-	_, dy = lib.draw_text(cr, fonts.text, ALIGNL, xw, yw, widget.weather.description)
-
-	yw = yw + dy + line_spacing
-
-	lib.draw_text(cr, fonts.caption, ALIGNL, xw, yw, widget.weather.location)
+	lib.draw_text(cr, fonts.text, ALIGNL, widget.weather.description_x, widget.weather.description_y, widget.weather.description)
+	lib.draw_text(cr, fonts.caption, ALIGNL, widget.weather.location_x, widget.weather.location_y, widget.weather.location)
 
 	-- Destroy cairo context
 	cairo_destroy(cr)
