d9d78a603bb661925a86c273be2b1383fb80948f
drakkar1969 <nicolabay@outlook.com>
2026-03-07 09:43:07 +0100

    Fix size and alignment in nothing rings conky

--- nothing/rings.lua
+++ nothing/rings.lua
@@ -23,100 +23,84 @@ local layouts = {
 ------------------------------------------------------------------------------
 -- WIDGET DATA
 ------------------------------------------------------------------------------
-local widgets = {}
-
-local widgets_style = {
+local widget = {
 	layout = layouts.COLUMN,
-	margin = 20
-}
-
----------------------------------------
--- CPU widget
----------------------------------------
-widgets[1] = {
-	heading = {
-		label = 'CPU'
-	},
-	ring = {
+	halign = lib.halign.LEFT,
+	valign = lib.valign.BOTTOM,
+	margin = 20,
+	rings = {
 		start_angle = -90,
 		end_angle = 90,
-		step = 9,
 		padding_x = 0,
 		outer_radius = 95,
-		mark_width = 12,
-		mark_thickness = 5,
-		label = '${cpu cpu0}%',
-		value = '${cpu cpu0}',
-		value_max = 100,
 		bg_color = lib.colors.default,
 		bg_alpha = 0.1,
 		fg_color = lib.colors.accent,
 		fg_alpha = 1
 	},
+	items = {}
+}
+
+---------------------------------------
+-- CPU widget
+---------------------------------------
+widget.items[1] = {
+	heading = {
+		label = 'CPU'
+	},
+	ring = {
+		step = 9,
+		mark_width = 12,
+		mark_thickness = 5,
+		label = '${cpu cpu0}%',
+		value = '${cpu cpu0}',
+		value_max = 100,
+	},
 	text = {
-		items = {
-			{ label = 'CORE', value = '${hwmon coretemp temp 1}°C' }
-		}
+		label = 'CORE',
+		value = '${hwmon coretemp temp 1}°C'
 	}
 }
 
 ---------------------------------------
 -- MEMORY widget
 ---------------------------------------
-widgets[2] = {
+widget.items[2] = {
 	heading = {
 		label = 'MEMORY'
 	},
 	ring = {
-		start_angle = -90,
-		end_angle = 90,
 		step = 9,
-		padding_x = 0,
-		outer_radius = 95,
 		mark_width = 12,
 		mark_thickness = 5,
 		label = '${memperc}%',
 		value = '${memperc}',
 		value_max = 100,
-		bg_color = lib.colors.default,
-		bg_alpha = 0.1,
-		fg_color = lib.colors.accent,
-		fg_alpha = 1
 	},
 	text = {
-		items = {
-			{ label = 'SWAP', value = '${swap}' }
-		}
+		label = 'SWAP',
+		value = '${swap}'
 	}
 }
 
 ---------------------------------------
 -- DISK widget
 ---------------------------------------
-widgets[3] = {
+widget.items[3] = {
 	heading = {
 		label = 'DISK'
 	},
 	ring = {
-		start_angle = -90,
-		end_angle = 90,
 		step = 9,
-		padding_x = 0,
-		outer_radius = 95,
 		mark_width = 12,
 		mark_thickness = 5,
 		label = '${fs_used_perc /home}%',
 		value = '${fs_used_perc /home}',
 		value_max = 100,
-		bg_color = lib.colors.default,
-		bg_alpha = 0.1,
-		fg_color = lib.colors.accent,
-		fg_alpha = 1
 	},
 	text = {
-		items = {
-			{ label = 'IO', value = '${diskio}' }
-		}
+		label = 'IO',
+		value = '${diskio}'
 	}
 }
 
@@ -126,30 +110,21 @@ widgets[3] = {
 local interface = 'wlp0s20f3'
 local wifi_max = 36000
 
-widgets[4] = {
+widget.items[4] = {
 	heading = {
 		label = 'WIRELESS'
 	},
 	ring = {
-		start_angle = -90,
-		end_angle = 90,
 		step = 9,
-		padding_x = 0,
-		outer_radius = 95,
 		mark_width = 12,
 		mark_thickness = 5,
 		label = '${downspeed '..interface..'}',
 		value = '${downspeedf '..interface..'}',
 		value_max = wifi_max,
-		bg_color = lib.colors.default,
-		bg_alpha = 0.1,
-		fg_color = lib.colors.accent,
-		fg_alpha = 1
 	},
 	text = {
-		items = {
-			{ label = '${wireless_essid '..interface..'}', value = '' }
-		}
+		label = '${wireless_essid '..interface..'}',
+		value = ''
 	}
 }
 
@@ -160,32 +135,88 @@ widgets[4] = {
 -- Function init_widgets
 ---------------------------------------
 function init_widgets()
-	for i, w in ipairs(widgets) do
-		w.width = (lib.bg.padding_x + w.ring.padding_x + w.ring.outer_radius) * 2
-		w.height = lib.fonts.heading.height + lib.line_spacing * 3.5 + w.ring.outer_radius + lib.fonts.text.height * #w.text.items + lib.line_spacing * (#w.text.items - 1) + lib.bg.padding_y * 2
+	-- Calculate item width and height
+	local item_w = (lib.bg.padding_x + widget.rings.padding_x + widget.rings.outer_radius) * 2
+	local item_h = lib.fonts.heading.height + lib.line_spacing * 3.5 + widget.rings.outer_radius + lib.fonts.text.height + lib.bg.padding_y * 2
 
-		if widgets_style.layout == layouts.COLUMN then
-			w.x = 0
-			w.y = (i - 1) * (w.height + widgets_style.margin)
-		elseif widgets_style.layout == layouts.BOX then
-			w.x = (i%2 == 1 and 0 or w.width + widgets_style.margin)
-			w.y = (i <= 2 and 0 or w.height + widgets_style.margin)
-		else
-			w.x = (i - 1) * (w.width + widgets_style.margin)
-			w.y = 0
+	-- Calculate widget x,y coordinates
+	local x = 0
+	local y = 0
+
+	if widget.layout == layouts.COLUMN then
+		if widget.halign == lib.halign.CENTER then
+			x = (conky_window.width - item_w)/2
+		elseif widget.halign == lib.halign.RIGHT then
+			x = conky_window.width - item_w
 		end
 
-		w.heading.x = w.x + w.width/2
-		w.heading.y = w.y + lib.bg.padding_y
+		if widget.valign == lib.valign.MIDDLE then
+			y = (conky_window.height - item_h * #widget.items - widget.margin * (#widget.items - 1))/2
+		elseif widget.valign == lib.valign.BOTTOM then
+			y = conky_window.height - item_h * #widget.items - widget.margin * (#widget.items - 1)
+		end
+	elseif widget.layout == layouts.BOX then
+		if widget.halign == lib.halign.CENTER then
+			x = (conky_window.width - item_w * 2 - widget.margin)/2
+		elseif widget.halign == lib.halign.RIGHT then
+			x = conky_window.width - item_w * 2 - widget.margin
+		end
 
-		w.ring.x = w.heading.x
-		w.ring.y = w.heading.y + lib.fonts.heading.height + lib.line_spacing * 1.5 + w.ring.outer_radius
+		if widget.valign == lib.valign.MIDDLE then
+			y = (conky_window.height - item_h * 2 - widget.margin)/2
+		elseif widget.valign == lib.valign.BOTTOM then
+			y = conky_window.height - item_h * 2 - widget.margin
+		end
+	else
+		if widget.halign == lib.halign.CENTER then
+			x = (conky_window.width - item_w * #widget.items - widget.margin * (#widget.items - 1))/2
+		elseif widget.halign == lib.halign.RIGHT then
+			x = conky_window.width - item_w * #widget.items - widget.margin * (#widget.items - 1)
+		end
 
-		w.ring.inner_radius = w.ring.outer_radius - w.ring.mark_width - w.ring.mark_thickness
+		if widget.valign == lib.valign.MIDDLE then
+			y = (conky_window.height - item_h)/2
+		elseif widget.valign == lib.valign.BOTTOM then
+			y = conky_window.height - item_h
+		end
+	end
 
-		w.text.xs = w.x + lib.bg.padding_x
-		w.text.xe = w.x + w.width - lib.bg.padding_x
-		w.text.y = w.ring.y + lib.line_spacing * 2
+	-- Calculate item data
+	for i, item in ipairs(widget.items) do
+		item.width = item_w
+		item.height = item_h
+
+		if widget.layout == layouts.COLUMN then
+			item.x = x
+			item.y = y + (i - 1) * (item_h + widget.margin)
+		elseif widget.layout == layouts.BOX then
+			item.x = x + (i%2 == 1 and 0 or item_w + widget.margin)
+			item.y = y + (i <= 2 and 0 or item_h + widget.margin)
+		else
+			item.x = x + (i - 1) * (item_w + widget.margin)
+			item.y = y
+		end
+
+		item.ring.start_angle = widget.rings.start_angle
+		item.ring.end_angle = widget.rings.end_angle
+		item.ring.padding_x = widget.rings.padding_x
+		item.ring.outer_radius = widget.rings.outer_radius
+		item.ring.bg_color = widget.rings.bg_color
+		item.ring.bg_alpha = widget.rings.bg_alpha
+		item.ring.fg_color = widget.rings.fg_color
+		item.ring.fg_alpha = widget.rings.fg_alpha
+
+		item.ring.inner_radius = item.ring.outer_radius - item.ring.mark_width - item.ring.mark_thickness
+
+		item.heading.x = item.x + item_w/2
+		item.heading.y = item.y + lib.bg.padding_y
+
+		item.ring.x = item.heading.x
+		item.ring.y = item.heading.y + lib.fonts.heading.height + lib.line_spacing * 1.5 + item.ring.outer_radius
+
+		item.text.xs = item.x + lib.bg.padding_x
+		item.text.xe = item.x + item_w - lib.bg.padding_x
+		item.text.y = item.ring.y + lib.line_spacing * 2
 	end
 end
 
@@ -193,7 +224,11 @@ end
 -- MAIN FUNCTION
 ------------------------------------------------------------------------------
 function conky_main()
+	local updates = tonumber(conky_parse('${updates}'))
+
 	if conky_window == nil then return end
+
+	if updates < 2 then return end
 
 	-- Create cairo context
 	local cs = cairo_xlib_surface_create(conky_window.display, conky_window.drawable, conky_window.visual, conky_window.width, conky_window.height)
@@ -208,8 +243,10 @@ function conky_main()
 
 		lib.update_font_colors(accent_color)
 
-		for i, w in pairs(widgets) do
-			w.ring.fg_color = accent_color
+		widget.rings.fg_color = accent_color
+
+		for _, item in pairs(widget.items) do
+			item.ring.fg_color = accent_color
 		end
 	end
 
@@ -221,7 +258,7 @@ function conky_main()
 		init_done = true
 	end
 
-	for i, w in pairs(widgets) do
+	for i, w in pairs(widget.items) do
 		-- Draw background
 		lib.draw_background(cr, w)
 
@@ -232,12 +269,8 @@ function conky_main()
 		lib.draw_ring(cr, w.ring)
 
 		-- Draw text
-		for i, item in pairs(w.text.items) do
-			local y = w.text.y + (lib.line_spacing + lib.fonts.text.height) * (i - 1)
-
-			lib.draw_text(cr, lib.fonts.text, lib.halign.LEFT, w.text.xs, y, item.label, w.text.xe - w.text.xs)
-			lib.draw_text(cr, lib.fonts.text, lib.halign.RIGHT, w.text.xe, y, item.value, w.text.xe - w.text.xs)
-		end
+		lib.draw_text(cr, lib.fonts.text, lib.halign.LEFT, w.text.xs, w.text.y, w.text.label, w.text.xe - w.text.xs)
+		lib.draw_text(cr, lib.fonts.text, lib.halign.RIGHT, w.text.xe, w.text.y, w.text.value, w.text.xe - w.text.xs)
 	end
 
 	-- Destroy cairo context
